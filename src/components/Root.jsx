9e3ccc439f2cfca6fef00b6a577becafbd05bc8c
Andrey Yamanov <tenphi@gmail.com>
2020-07-22 16:52:04 +0300

    update font definition

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -5,12 +5,14 @@ export default function Root(props) {
     responsive="80rem|60rem|40rem"
     fill="bg"
     height="min 100vh"
-    style={{ 'fontFamily': 'CeraPro, "Avenir Next", "Avenir", Helvetica, Ubuntu, "DejaVu Sans", Arial, sans-serif' }}>
+    text="n">
     <nu-theme hue="269" saturation="90"></nu-theme>
     <nu-theme name="primary" hue="269" saturation="90" mod="tint"></nu-theme>
     <nu-theme name="secondary" hue="359" saturation="84" mod="tint"></nu-theme>
     <nu-theme name="aside" hue="269" saturation="25" mod="strong special"></nu-theme>
-    <nu-props radius=".5x"></nu-props>
+    <nu-props
+      radius=".5x"
+      font="CeraPro, 'Avenir Next', 'Avenir', Helvetica, Ubuntu, 'DejaVu Sans', Arial, sans-serif"/>
     <nu-attrs for="btn" theme="primary tint :red[secondary tint]"></nu-attrs>
     { props.children }
   </nu-root>;
