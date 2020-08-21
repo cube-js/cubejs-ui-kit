95b431aaaf4e0fed2ce07615d2fa61b0bb010f1d
Andrey Yamanov <tenphi@gmail.com>
2020-08-21 19:58:33 +0300

    Fix Hero & SiteBlock paddings

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
 import SiteBlock from './SiteBlock.jsx';
-import { attrs } from '../helpers';
 
 export default function Hero(props) {
   return <SiteBlock
-    badge={props.badge}
-    wide={props.wide}
-    githubBadge={props.githubBadge}
-    image={props.image}
-    bgImage={props.bgImage}
-    heading={props.heading}
-    description={props.description}
     level={1}
-    headingSize={props.headingSize}
-    {...attrs(props)}
+    {...props}
   >
     {props.children}
   </SiteBlock>;
