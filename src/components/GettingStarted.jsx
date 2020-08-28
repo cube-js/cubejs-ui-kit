8259a1dcbf141bbc360c54da7d103f2f839971a8
Andrey Yamanov <tenphi@gmail.com>
2020-08-28 14:16:25 +0300

    GettingStarted: fix snippet width on iOS

--- src/components/GettingStarted.jsx
+++ src/components/GettingStarted.jsx
@@ -13,7 +13,7 @@ export default function GettingStarted(props) {
     {...attrs(props)}>
     <nu-flex width="max --content-width" gap>
       <nu-card
-        display="grid" padding="0 8x||0 2x" content="center start"
+        display="grid" padding="0 8x||0 2x" content="center start" width="max --content-width"
         text="monospace nowrap" fill="bg" as="h4" overflow="auto hidden" radius border="#pink-04">
         $ { props.command || INSTALL_COMMAND }
       </nu-card>
