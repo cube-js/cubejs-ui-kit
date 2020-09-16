727eb2bd632b4f61d3be82ed02b6d1a7f671b14a
Andrey Yamanov <tenphi@gmail.com>
2020-09-16 09:34:18 +0300

    fix form submit

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -3,9 +3,9 @@ export default {
     const response = await fetch(url, {
       method: 'POST',
       headers: {
-        'Accept': 'application/json'
+        'Accept': 'application/json',
       },
-      data: JSON.stringify(data),
+      body: JSON.stringify(data),
     });
 
     return response.json();
