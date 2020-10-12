19d0919ef06b022a4921e464559cc1fd5ee3d3f8
Leonid Yakovlev <leonid@cube.dev>
2020-10-12 17:44:53 +0300

    ExploreCubeCloud: only one icon

--- src/logos.js
+++ src/logos.js
@@ -1,3 +1,2 @@
 export { default as main } from './assets/logos/cubejs.svg';
 export { default as cubeCloud } from './assets/logos/cubecloud.svg';
-export { default as cubeCloudDefaultIcon } from './assets/logos/cube_cloud.svg';
